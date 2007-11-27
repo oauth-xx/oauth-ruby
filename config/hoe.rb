637ce4f28911814b67a706d41f8a54b207328196
Pelle Braendgaard <pelleb@gmail.com>
2007-11-27 06:19:43 +0000

    Fixed a problem with the dependencies.

--- config/hoe.rb
+++ config/hoe.rb
@@ -59,7 +59,7 @@ hoe = Hoe.new(GEM_NAME, VERS) do |p|
   
   # == Optional
   p.changes = p.paragraphs_of("History.txt", 0..1).join("\n\n")
-  p.extra_deps = ['ruby-hmac','>= 0.3.1']     # An array of rubygem dependencies [name, version], e.g. [ ['active_support', '>= 1.3.1'] ]
+  p.extra_deps = [['ruby-hmac','>= 0.3.1'] ]    # An array of rubygem dependencies [name, version], e.g. [ ['active_support', '>= 1.3.1'] ]
   
   #p.spec_extras = {}    # A hash of extra values to set in the gemspec.
   
