b11f196b7563a5a283ff499447202240b8666740
Seth Fitzsimmons <seth@mojodna.net>
2009-01-21 20:22:00 -0800

    describe how an OAuth::RequestProxy::MockRequest is used

--- lib/oauth/request_proxy/mock_request.rb
+++ lib/oauth/request_proxy/mock_request.rb
@@ -2,6 +2,21 @@ require 'oauth/request_proxy/base'
 
 module OAuth
   module RequestProxy
+    # RequestProxy for Hashes to facilitate simpler signature creation.
+    # Usage:
+    #   request = OAuth::RequestProxy.proxy \
+    #      "method" => "iq",
+    #      "uri"    => [from, to] * "&",
+    #      "parameters" => {
+    #        "oauth_consumer_key"     => oauth_consumer_key,
+    #        "oauth_token"            => oauth_token,
+    #        "oauth_signature_method" => "HMAC-SHA1"
+    #      }
+    #
+    #   signature = OAuth::Signature.sign \
+    #     request,
+    #     :consumer_secret => oauth_consumer_secret,
+    #     :token_secret    => oauth_token_secret,
     class MockRequest < OAuth::RequestProxy::Base
       proxies Hash
 
