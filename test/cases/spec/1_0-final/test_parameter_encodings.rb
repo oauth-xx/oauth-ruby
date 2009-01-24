3c8126c56469e6580a3db335ad6750eb566469a0
Seth Fitzsimmons <seth@mojodna.net>
2009-01-23 17:22:08 -0800

    fixed encoding tests

--- test/cases/spec/1_0-final/test_parameter_encodings.rb
+++ test/cases/spec/1_0-final/test_parameter_encodings.rb
@@ -13,53 +13,74 @@ require File.dirname(__FILE__) + '/../../oauth_case'
 # 
 
 class ParameterEncodingTest < OAuthCase
-  include OAuth::Helper
-  
   def test_encodings_alpha_num
-    assert_encoding('abcABC123','abcABC123')
+    assert_encoding 'abcABC123', 'abcABC123'
   end
-  
+
   def test_encodings_non_escaped
-    assert_encoding('-._~','-._~')
+    assert_encoding '-._~', '-._~'
   end
 
   def test_encodings_percent
-    assert_encoding('%25','%')
+    assert_encoding '%25', '%'
   end
 
   def test_encodings_plus
-    assert_encoding('%2B','+')
+    assert_encoding '%2B', '+'
+  end
+
+  def test_encodings_space
+    assert_encoding '%20', ' '
   end
 
   def test_encodings_query_param_symbols
-    assert_encoding('%26%3D%2A','&=*')
+    assert_encoding '%26%3D%2A', '&=*'
   end
 
-  def test_encodings_utf_lf
-    assert_encoding('U+000A','%0A')
+  def test_encodings_unicode_lf
+    assert_encoding '%0A', unicode_to_utf8('U+000A')
   end
 
-  def test_encodings_utf_space
-    assert_encoding('U+0020','%20')
+  def test_encodings_unicode_space
+    assert_encoding '%20', unicode_to_utf8('U+0020')
   end
 
-  def test_encodings_utf_007f
-    assert_encoding('U+007F','%7F')
+  def test_encodings_unicode_007f
+    assert_encoding '%7F', unicode_to_utf8('U+007F')
   end
 
-  def test_encodings_utf_0080
-    assert_encoding('U+0080','%C2%80')
+  def test_encodings_unicode_0080
+    assert_encoding '%C2%80', unicode_to_utf8('U+0080')
   end
 
-  def test_encodings_utf_3001
-    assert_encoding('U+3001','%E3%80%81')
+  def test_encoding_unicode_2708
+    assert_encoding '%E2%9C%88', unicode_to_utf8('U+2708')
   end
-  
-  protected
-  
-  
-  def assert_encoding(expected,given,message=nil)
-    assert_equal expected,escape(given) , message
+
+  def test_encodings_unicode_3001
+    assert_encoding '%E3%80%81', unicode_to_utf8('U+3001')
   end
-  
+
+protected
+
+  def unicode_to_utf8(unicode)
+    return unicode if unicode =~ /\A[[:space:]]*\z/m
+
+    str = ''
+
+    unicode.scan(/(U\+(?:[[:digit:][:xdigit:]]{4,5}|10[[:digit:][:xdigit:]]{4})|.)/mu) do
+      c = $1
+      if c =~ /^U\+/
+        str << [c[2..-1].hex].pack('U*')
+      else
+        str << c
+      end
+    end
+
+    str
+  end
+
+  def assert_encoding(expected, given, message = nil)
+    assert_equal expected, OAuth::Helper.escape(given), message
+  end
 end
