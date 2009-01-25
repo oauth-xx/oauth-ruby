f49fe790885e13929d0e58afc11776c54b228728
Seth Fitzsimmons <seth@mojodna.net>
2009-01-24 17:09:22 -0800

    use redgreen for test output where available

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,14 @@
 require 'test/unit'
 require File.dirname(__FILE__) + '/../lib/oauth'
 
+begin
+  # load redgreen unless running from within TextMate (in which case ANSI
+  # color codes mess with the output)
+  require 'redgreen' unless ENV['TM_CURRENT_LINE']
+rescue LoadError
+  nil
+end
+
 def requests(request)
   Marshal.load(File.read(File.dirname(__FILE__) + '/fixtures/' + request))
 end
