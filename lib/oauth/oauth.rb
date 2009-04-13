470c08ec3a9b55e85a6f5bbc730d387f3bf7afa2
Matt Sanford <matt@twitter.com>
2009-04-13 11:54:31 -0700

    Fix the reserved characters regex to be non-variant by $KCODE

--- lib/oauth/oauth.rb
+++ lib/oauth/oauth.rb
@@ -3,5 +3,5 @@ module OAuth
   PARAMETERS = %w(oauth_consumer_key oauth_token oauth_signature_method oauth_timestamp oauth_nonce oauth_version oauth_signature)
 
   # reserved character regexp, per section 5.1
-  RESERVED_CHARACTERS = /[^\w\d\-\.\_\~]/
+  RESERVED_CHARACTERS = /[^a-zA-Z0-9\-\.\_\~]/
 end
--- test/test_signature.rb
+++ test/test_signature.rb
@@ -2,10 +2,18 @@ require File.dirname(__FILE__) + '/test_helper.rb'
 
 class TestOauth < Test::Unit::TestCase
 
-  def setup
-  end
-  
-  def test_truth
-    assert true
+  def test_parameter_escaping_kcode_invariant
+    old = $KCODE
+    begin
+      %w(n N e E s S u U).each do |kcode|
+        $KCODE = kcode
+        assert_equal '%E3%81%82', OAuth::Helper.escape('あ'),
+                      "Failed to correctly escape Japanese under $KCODE = #{kcode}"
+        assert_equal '%C3%A9', OAuth::Helper.escape('é'),
+                      "Failed to correctly escape e+acute under $KCODE = #{kcode}"
+      end
+    ensure
+      $KCODE = old
+    end
   end
 end
