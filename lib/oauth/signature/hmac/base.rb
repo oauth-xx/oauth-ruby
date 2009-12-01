00c74be340aaaa7e192ccc60cda6e4e7ef5616cf
Aaron Quint <aaron@quirkey.com>
2009-12-01 12:04:31 -0500

    Cleaning up unused code

--- lib/oauth/signature/hmac/base.rb
+++ lib/oauth/signature/hmac/base.rb
@@ -1,24 +1,15 @@
 # -*- encoding: utf-8 -*-
 
 require 'oauth/signature/base'
+require 'digest/hmac'
 
 module OAuth::Signature::HMAC
   class Base < OAuth::Signature::Base
 
   private
-
     def digest
-      # if RUBY_VERSION =~ /^1\.9/
-        # use stdlib
-        require 'digest/hmac'
-        self.class.digest_class Object.module_eval("::Digest::#{self.class.digest_klass}")
-        Digest::HMAC.digest(signature_base_string, secret, self.class.digest_class)
-      # else 
-      #   # use gem
-      #   require self.class.implements
-      #   self.class.digest_class Object.module_eval("::HMAC::#{self.class.digest_klass}")
-      #   self.class.digest_class.digest(secret, signature_base_string)
-      # end
+      self.class.digest_class Object.module_eval("::Digest::#{self.class.digest_klass}")
+      Digest::HMAC.digest(signature_base_string, secret, self.class.digest_class)
     end
   end
 end
