08191d99389dcc0a494e7467ee13387e5b4c386b
Philip (flip) Kromer <flip@infochimps.org>
2009-12-09 03:30:54 +0000

    Don't let client/helper.rb override the oauth_version

--- lib/oauth/client/helper.rb
+++ lib/oauth/client/helper.rb
@@ -36,7 +36,7 @@ module OAuth::Client
         'oauth_timestamp'        => timestamp,
         'oauth_nonce'            => nonce,
         'oauth_verifier'         => options[:oauth_verifier],
-        'oauth_version'          => '1.0'
+        'oauth_version'          => (options[:oauth_version] || '1.0')
       }.reject { |k,v| v.to_s == "" }
     end
 
