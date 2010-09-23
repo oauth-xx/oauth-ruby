ab91191183df24438b561217709f69bc955749e9
Steven Parkes <smparkes@smparkes.net>
2010-09-23 15:00:20 -0700

    add scope to keep google happy

--- lib/oauth/client/helper.rb
+++ lib/oauth/client/helper.rb
@@ -28,6 +28,7 @@ module OAuth::Client
 
     def oauth_parameters
       {
+        'scope'                  => options[:scope],
         'oauth_body_hash'        => options[:body_hash],
         'oauth_callback'         => options[:oauth_callback],
         'oauth_consumer_key'     => options[:consumer].key,
