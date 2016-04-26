5c595720cc4baee2dc8214c59c9ddabf1d010b3d
James Pinto <thejamespinto@gmail.com>
2016-04-26 01:40:44 -0300

    codeclimate

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,12 @@
+ENV['RACK_ENV'] = 'test'
+
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 require 'rubygems'
 require 'minitest/autorun'
 require 'mocha/mini_test'
 require 'rack/test'
-
-ENV['RACK_ENV'] = 'test'
 
 require 'byebug'
 
@@ -11,5 +14,6 @@ $LOAD_PATH << File.dirname(__FILE__) + '/../lib/'
 require 'oauth'
 require 'stringio'
 require 'webmock/minitest'
+WebMock.disable_net_connect!(allow: "codeclimate.com")
 
 require 'support/minitest_helpers'
