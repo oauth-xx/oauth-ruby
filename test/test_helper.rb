d287620115c11193b41bfec91b313c0027426e14
James Pinto <thejamespinto@gmail.com>
2016-04-28 14:48:40 -0300

    making a test/support dir

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,21 +12,4 @@ require 'oauth'
 require 'stringio'
 require 'webmock/minitest'
 
-Minitest::Test.class_eval do
-
-  def assert_matching_headers(expected, actual)
-    # transform into sorted arrays
-    auth_intro, auth_params = actual.split(' ', 2)
-    assert_equal auth_intro, 'OAuth'
-    expected    = expected.split(/(,|\s)/).reject {|v| v == '' || v =~ /^[\,\s]+/}.sort
-    auth_params = auth_params.split(/(,|\s)/).reject {|v| v == '' || v =~ /^[\,\s]+/}.sort
-    assert_equal expected, auth_params
-  end
-
-  def stub_test_ie
-    stub_request(:any, "http://term.ie/oauth/example/request_token.php").to_return(:body => "oauth_token=requestkey&oauth_token_secret=requestsecret")
-    stub_request(:post, "http://term.ie/oauth/example/access_token.php").to_return(:body => "oauth_token=accesskey&oauth_token_secret=accesssecret")
-    stub_request(:get, %r{http://term\.ie/oauth/example/echo_api\.php\?.+}).to_return(lambda {|request| {:body => request.uri.query}})
-    stub_request(:post, "http://term.ie/oauth/example/echo_api.php").to_return(lambda {|request| {:body => request.body}})
-  end
-end
+require 'support/minitest_helpers'
