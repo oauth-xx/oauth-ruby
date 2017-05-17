2c8ecdab4cac170eba359b58622f3f452a34b520
James Pinto <thejamespinto@gmail.com>
2017-05-17 17:55:10 -0300

    quickfix

--- test/units/test_cli.rb
+++ test/units/test_cli.rb
@@ -58,7 +58,7 @@ class TestCLI < Minitest::Test
   def test_version
     out = run_command(%w[version])
 
-    assert_equal "OAuth for Ruby 0.5.1\n", out
+    assert_equal "OAuth for Ruby 0.5.2\n", out
   end
 
   def test_query_empty
