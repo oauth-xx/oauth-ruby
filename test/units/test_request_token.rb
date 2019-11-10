c7e95ca213e682172b6c177f118c866fc6750bd1
Agora@Ubuntu-dev <filter@agora-security.com>
2019-11-10 11:50:21 -0800

    Fix specs for test_request_token

--- test/units/test_request_token.rb
+++ test/units/test_request_token.rb
@@ -1,8 +1,8 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class StubbedToken < OAuth::RequestToken
-  define_method :build_authorize_url_promoted do |root_domain, params|
-    build_authorize_url root_domain, params
+  define_method :build_url_promoted do |root_domain, params|
+    build_url root_domain, params
   end
 end
 
@@ -40,14 +40,38 @@ class TestRequestToken < Minitest::Test
     assert_nil @request_token.authorize_url
   end
 
+  def test_request_token_builds_authenticate_url_connectly_with_additional_params
+    authenticate_url = @request_token.authenticate_url({:oauth_callback => "github.com"})
+    assert authenticate_url
+    assert_match(/oauth_token/, authenticate_url)
+    assert_match(/oauth_callback/, authenticate_url)
+  end
+
+  def test_request_token_builds_authenticate_url_connectly_with_no_or_nil_params
+    # we should only have 1 key in the url returned if we didn't pass anything.
+    # this is the only required param to authenticate the client.
+    authenticate_url = @request_token.authenticate_url(nil)
+    assert authenticate_url
+    assert_match(/\?oauth_token=/, authenticate_url)
+
+    authenticate_url2 = @request_token.authenticate_url
+    assert authenticate_url2
+    assert_match(/\?oauth_token=/, authenticate_url2)
+  end
+
+  def test_request_token_returns_nil_authenticate_url_when_token_is_nil
+    @request_token.token = nil
+    assert_nil @request_token.authenticate_url
+  end
+
   #TODO: mock out the Consumer to test the Consumer/AccessToken interaction.
   def test_get_access_token
   end
 
-  def test_build_authorize_url
+  def test_build_url
    @stubbed_token = StubbedToken.new(nil, nil, nil)
-    assert_respond_to @stubbed_token, :build_authorize_url_promoted
-    url = @stubbed_token.build_authorize_url_promoted(
+    assert_respond_to @stubbed_token, :build_url_promoted
+    url = @stubbed_token.build_url_promoted(
       "http://github.com/oauth/authorize",
       {:foo => "bar bar"})
     assert url
