9923c5706bfb63cae813d4e1c09c82ee994721ab
Jeremy Sioui <jeremy.sioui@gmail.com>
2021-02-10 15:17:09 -0400

    Add OAuth::Signature::HMAC::SHA256 and associated tests

--- lib/oauth.rb
+++ lib/oauth.rb
@@ -7,5 +7,6 @@ require 'oauth/oauth'
 
 require 'oauth/client/helper'
 require 'oauth/signature/hmac/sha1'
+require 'oauth/signature/hmac/sha256'
 require 'oauth/signature/rsa/sha1'
 require 'oauth/request_proxy/mock_request'
